a552de3e7dad3436a57ef8211c39d60badc5bda1
Will Lewis <will@wflewis.com>
2017-10-06 01:03:36 -0400

    Use SDL and implement a simple popup

--- src/sprout.cr
+++ src/sprout.cr
@@ -1,5 +1,20 @@
+require "sdl"
+require "sdl/ttf"
+
 require "./sprout/*"
 
+SDL.init(SDL::Init::VIDEO)
+SDL::TTF.init; at_exit { SDL::TTF.quit }
+
 module Sprout
-  # TODO Put your code here
+  GREEN          = SDL::Color[128, 160, 128, 255]
+  DARK           = SDL::Color[45, 42, 46, 255]
+  LIGHT          = SDL::Color[252, 252, 250, 255]
+  FONT_HELVETICA = SDL::TTF::Font.new("/System/Library/Fonts/Helvetica.ttc", 35)
+end
+
+window = Sprout::Window.new
+
+window.draw do |w|
+  w.draw_left_text("hello")
 end
---
+++ src/sprout/window.cr
@@ -0,0 +1,47 @@
+module Sprout
+  class Window
+    @background : SDL::Color
+    @foreground : SDL::Color
+
+    def initialize
+      at_exit { SDL.quit }
+      @window = SDL::Window.new("Sprout", 600, 80)
+      @window.bordered = false
+      @renderer = SDL::Renderer.new(@window)
+      @background = Sprout::GREEN
+      @foreground = Sprout::LIGHT
+    end
+
+    def draw
+      loop do
+        case event = SDL::Event.wait
+        when SDL::Event::Quit
+          break
+        end
+
+        clear
+        yield self
+        @renderer.present
+      end
+    end
+
+    def clear
+      @renderer.draw_color = @background
+      @renderer.clear
+    end
+
+    def draw_centered_text(string : String)
+      surface = Sprout::FONT_HELVETICA.render_shaded(string, @foreground, @background)
+      x = (@window.width - surface.width) / 2
+      y = (@window.height - surface.height) / 2
+      @renderer.copy(surface, dstrect: SDL::Rect[x, y, surface.width, surface.height])
+    end
+
+    def draw_left_text(string : String)
+      surface = Sprout::FONT_HELVETICA.render_shaded(string, @foreground, @background)
+      x = 25
+      y = (@window.height - surface.height) / 2
+      @renderer.copy(surface, dstrect: SDL::Rect[x, y, surface.width, surface.height])
+    end
+  end
+end
