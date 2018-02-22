0978e747b9bc0e95053b3e6f71a394296831d3bf
Will Lewis <will@wflewis.com>
2018-02-22 15:55:39 -0500

    Add fonts and basic commands

--- src/sprout.cr
+++ src/sprout.cr
@@ -1,5 +1,6 @@
 require "sdl"
 require "sdl/ttf"
+require "nth"
 
 require "./sprout/*"
 
@@ -10,8 +11,8 @@ module Sprout
   GREEN          = SDL::Color[128, 160, 128, 255]
   DARK           = SDL::Color[45, 42, 46, 255]
   LIGHT          = SDL::Color[252, 252, 250, 255]
-  FONT_HELVETICA = SDL::TTF::Font.new("/System/Library/Fonts/Helvetica.ttc", 35)
-  FONT_SCPRO     = SDL::TTF::Font.new("/Users/will/Library/Fonts/SourceCodePro-Regular.ttf", 35)
+  FONT_HELVETICA = SDL::TTF::Font.new("#{__DIR__}/../res/Helvetica.ttc", 35)
+  FONT_SCPRO     = SDL::TTF::Font.new("#{__DIR__}/../res/SourceCodePro-Regular.ttf", 35)
 end
 
 window = Sprout::Window.new
--- src/sprout/commander.cr
+++ src/sprout/commander.cr
@@ -5,7 +5,27 @@ module Sprout
     end
 
     def run
-      puts "running #{@command}"
+      tokens = @command.split(" ")
+      action = tokens[1.st]
+      params = tokens[2.nd..-1].join(" ")
+
+      case action
+      when "o", "open"
+        open params
+      when "t", "term"
+        term params
+      else
+        puts "running #{@command}"
+      end
+    end
+
+    def open(params)
+      puts "opening finder to: #{params}"
+      system("open -a Finder -- #{params}")
+    end
+
+    def term(params)
+      puts "running in terminal: #{params}"
     end
   end
 end
